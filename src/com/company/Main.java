d058cda0951a7e82e69f794e58ff22a14de4e7af
MARK-PC\Mark <bigbluethe2@gmail.com>
2018-08-30 18:51:18 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello, My name is Mark");
+	    System.out.println("I've had an interest in programming since I was 10 years old");
+	    System.out.println("The first programming language I worked with was BASIC");
+	    System.out.println("I made a Rock, Paper, Scissors game with it");
+    }
+}
